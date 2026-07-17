62f17bcbe9f39e6b065accbe3e253aeb78bda2ca
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-07-17 20:55:25 +0530

    Manufacturing section completed

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ window.addEventListener("scroll", () => {
 })
 
 
+
+//fixed btn
+
+const fixedbtn=document.querySelector(".fixed-btn")
+fixedbtn.addEventListener("click",()=>{
+    window.scrollTo({
+        top:"0px",
+        behavior:"smooth"
+    })
+})
+
+//
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>50){
+        fixedbtn.classList.add("active")
+    }
+
+    else{
+        fixedbtn.classList.remove("active")
+    }
+})
+
