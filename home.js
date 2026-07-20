c2944a9c3607c0f4d640546388d1050dfb302d71
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-07-20 09:07:21 +0530

    all pages header and footer added

---
+++ home.js
@@ -0,0 +1,21 @@
+
+//fixed btn
+
+const fixedbtn=document.querySelector(".fixed-btn")
+fixedbtn.addEventListener("click",()=>{
+    window.scrollTo({
+        top:"0px",
+        behavior:"smooth"
+    })
+})
+
+//
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>50){
+        fixedbtn.classList.add("active")
+    }
+
+    else{
+        fixedbtn.classList.remove("active")
+    }
+})
--- script.js
+++ script.js
@@ -26,24 +26,4 @@ window.addEventListener("scroll", () => {
 
 
 
-//fixed btn
-
-const fixedbtn=document.querySelector(".fixed-btn")
-fixedbtn.addEventListener("click",()=>{
-    window.scrollTo({
-        top:"0px",
-        behavior:"smooth"
-    })
-})
-
-//
-window.addEventListener("scroll",()=>{
-    if(window.scrollY>50){
-        fixedbtn.classList.add("active")
-    }
-
-    else{
-        fixedbtn.classList.remove("active")
-    }
-})
 
